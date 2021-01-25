d71e1aa5c7d57a26d137f1c49442b8dabdf49ff2
Sarthak Gupte <sarthakgupte@Sarthaks-MacBook-Air.local>
2021-01-25 03:43:11 -0500

    Updates

---
+++ precache-manifest.7e627ace5272352a71cde89fc0a394b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b85b6f14efa7107d8a38",
+    "url": "/my-website/static/css/main.60632195.chunk.css"
+  },
+  {
+    "revision": "b85b6f14efa7107d8a38",
+    "url": "/my-website/static/js/main.b85b6f14.chunk.js"
+  },
+  {
+    "revision": "37063c54184f9fd8244e",
+    "url": "/my-website/static/js/runtime~main.37063c54.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/my-website/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "963d3e258c1331f576ba567e5c318732",
+    "url": "/my-website/index.html"
+  }
+];
